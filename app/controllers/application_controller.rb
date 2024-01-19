018c1c8995cd7242365b485aff458164b031984c
kevin-ar_e <ayak57085@gmail.com>
2024-01-19 23:53:58 +0000

    update data of user_registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    #root pour revenir a la page d'acceuil apres connection !
+    def after_sign_in_path_for(resource)
+        projects_path
+    end
 end
